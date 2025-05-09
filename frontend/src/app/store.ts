571bbe28ec13e46791d05d6fb4ba3fa58bd35b79
Ainesh Sridhar <aineshsridhar@gmail.com>
2025-05-09 23:06:58 +0530

    Working on redux state mgmt

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import roomReducer from "../features/rooms/roomSlice";
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        rooms: roomReducer,
+    }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
